9f57bc3bffb297c26af8bffc082cb61a7fb43a3e
yerim110914-pixel <yerim110914@gmail.com>
2026-09-06 17:33:17 +0900

    V-17.1창크기조절
    V-17.1창크기조절

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE='the-vision-family-v16-10-1';
+const CACHE='the-vision-family-v16-13-1';
 const ASSETS=[
-  './manifest.webmanifest?v=16.10',
-  './icon-192.png?v=16.10',
-  './icon-512.png?v=16.10',
-  './favicon.png?v=16.10',
-  './vision-logo.png?v=16.10'
+  './manifest.webmanifest?v=16.13',
+  './icon-192.png?v=16.13',
+  './icon-512.png?v=16.13',
+  './favicon.png?v=16.13',
+  './vision-logo.png?v=16.13'
 ];
 
 self.addEventListener('install',e=>{
